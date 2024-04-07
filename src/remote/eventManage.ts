2542628147445de133d64859cdd6d88b4af8c55e
resetmerlin <resetmerlin@gmail.com>
2024-04-07 18:47:09 +0900

    Feat: getEventMutation 함수 추가

---
+++ src/remote/eventManage.ts
@@ -0,0 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import axios from 'axios';
+import { IMultipleEvent } from '../interface';
+import { isApiError, toErrorWithMessage } from '../utils';
+
+// eslint-disable-next-line import/prefer-default-export
+export const getEventMutation = (query: {
+  page: number;
+  perPage: number;
+}): Promise<IMultipleEvent> => {
+  return axios
+    .get(`api/event?page=${query.page}&perPage=${query.perPage}`)
+    .then((response) => response.data)
+    .catch((error) => {
+      if (isApiError(error)) {
+        const { data } = error.error;
+        reportError(data.message);
+      } else {
+        reportError(toErrorWithMessage(error).message);
+      }
+      return Promise.reject(error);
+    });
+};
