925ab0bfb231ed8ea5c3f503180495e3176a0765
resetmerlin <resetmerlin@gmail.com>
2024-04-07 13:41:54 +0900

    Feat: Dashboard Chart 컴포넌트 추가

---
+++ src/components/molecules/dashboardChart/DashboardChart.tsx
@@ -0,0 +1,39 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
+import { ChartContainer } from '../../atom';
+import { customColors } from '../../../styles';
+import { IMultipleDashboard } from '../../../interface';
+
+interface IProps {
+  data: IMultipleDashboard['data'];
+}
+export default function DashboardChart({ data }: IProps) {
+  return (
+    <ChartContainer title="회원가입 현황">
+      <ResponsiveContainer width="100%" aspect={1.8}>
+        <LineChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" tickFormatter={(date) => date.substring(5)} />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="linear"
+            dataKey="user"
+            stroke={customColors.main_blue}
+            name="회원가입 수"
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </ChartContainer>
+  );
+}
