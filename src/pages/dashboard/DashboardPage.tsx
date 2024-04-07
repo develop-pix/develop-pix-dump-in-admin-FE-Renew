133f7c6778f982962079c04cf98fa0cd89ffe6fa
resetmerlin <resetmerlin@gmail.com>
2024-04-07 14:39:54 +0900

    Feat: Sidebar 컴포넌트 Dashboard Page에 추가

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,13 +1,14 @@
 /* eslint-disable import/no-cycle */
 import { Box } from '@mui/material';
 import { useDashboardPage } from './DashboardPage.hook';
-import { DashboardChart, DashboardTable } from '../../components';
+import { DashboardChart, DashboardTable, Sidebar } from '../../components';
 
 export default function DashboardPage() {
   const { state } = useDashboardPage();
 
   return (
     <Box sx={{ display: 'flex' }}>
+      <Sidebar />
       <Box sx={{ display: 'flex' }}>
         <DashboardChart data={state.data} />
         <DashboardTable
