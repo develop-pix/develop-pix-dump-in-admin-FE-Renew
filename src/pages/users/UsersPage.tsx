f91ec773aed6a04ac8c258543379ca4257cce565
resetmerlin <resetmerlin@gmail.com>
2024-04-19 22:08:39 +0900

    Feat: 사이드 바 컴포넌트 추가

--- src/pages/users/UsersPage.tsx
+++ src/pages/users/UsersPage.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import { Box } from '@mui/material';
+import { Sidebar } from '../../components';
 
 export default function UsersPage() {
-  return <div>UsersPage</div>;
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Sidebar />
+    </Box>
+  );
 }
