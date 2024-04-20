39630ef060805899a4b7c773315d57b980278dfb
resetmerlin <resetmerlin@gmail.com>
2024-04-20 14:52:25 +0900

    Feat: useMutation fn에 사용될 유저들 가져오는 mutation 추가

---
+++ src/remote/userManage.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { IMultipleUserManage } from '../interface';
+import { isApiError, toErrorWithMessage } from '../utils';
+
+// eslint-disable-next-line import/prefer-default-export
+export const getMultipleUserMutation = (query: {
+  page: number;
+  perPage: number;
+}): Promise<IMultipleUserManage> => {
+  return axios
+    .get(`/api/user?page=${query.page}&perPage=${query.perPage}`)
+    .then((response) => response.data)
+    .catch((error) => {
+      if (isApiError(error)) {
+        const { data } = error.error;
+        reportError(data.message);
+      } else {
+        reportError(toErrorWithMessage(error).message);
+      }
+      return Promise.reject(error);
+    });
+};
